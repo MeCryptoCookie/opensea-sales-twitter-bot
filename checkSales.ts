e3d0db074722945936344cfa4f2795899d43261e
Steven Thewissen <info@getpixls.com>
2021-08-27 09:23:44 +0200

    Twitter bot fix

--- checkSales.ts
+++ checkSales.ts
@@ -44,7 +44,13 @@ async function formatAndSendTweet(sale: { asset: { name: any; permalink: any; de
 async function main() {
   const seconds = process.env.SECONDS ? parseInt(process.env.SECONDS) : 3_600;
   const hoursAgo = (Math.round(new Date().getTime() / 1000) - (seconds)); // in the last hour, run hourly?
-  
+  const settings = { 
+    method: "GET",
+    headers: {
+      "X-API-KEY": process.env.OPEN_SEA_API_KEY
+    }
+  };
+
   const openSeaResponse = await fetch(
     "https://api.opensea.io/api/v1/events?" + new URLSearchParams({
       offset: '0',
@@ -52,8 +58,7 @@ async function main() {
       event_type: 'successful',
       only_opensea: 'false',
       occurred_after: hoursAgo.toString(), 
-      collection_slug: process.env.COLLECTION_SLUG!,
-      contract_address: process.env.CONTRACT_ADDRESS!
+      collection_slug: process.env.COLLECTION_SLUG!
   })).then((resp) => resp.json());
 
   await Promise.all(
